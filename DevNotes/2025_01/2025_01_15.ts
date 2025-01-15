b72e62a5026f5479ba18f18e179ed9e72420826f
bulany <bulany.git@gmail.com>
2025-01-15 16:46:12 +0100

    Get some tick marks happening

---
+++ DevNotes/2025_01/2025_01_15.ts
@@ -0,0 +1,79 @@
+// 16h21: drew in book
+// Need file watcher so that when you save it automatically 
+// generates the svg or whatever...
+// Need to d3 data join instead of forEach
+import * as d3 from "npm:d3";
+import { JSDOM } from "npm:jsdom";
+
+const dom = new JSDOM('<!DOCTYPE html><html></html><body></body></html>');
+const document = dom.window.document;
+
+function writeFile(fileName : string, content : string) {
+  const currentFile = import.meta.url;
+  const currentDir = new URL('.', currentFile).pathname;
+  try {
+    Deno.writeTextFileSync(`${currentDir}${fileName}`, content);
+    console.log(`${fileName} created successfully!`);
+  } catch (error) {
+    console.error(`Error writing ${fileName}`, error);
+  }
+}
+
+const now = new Date();
+const formatDay = d3.timeFormat("%Y_%m_%d");
+const dayStr = formatDay(now);
+console.log('today', formatDay(now));
+
+const dayStart = d3.timeDay.floor(now);
+const dayStop = d3.timeDay.offset(dayStart, 1);
+const hours = d3.timeHour.range(dayStart, dayStop);
+hours.push(dayStop);
+const data = hours.map((hour : Date, i : number) => {
+  return {
+    i: i,
+    time: hour,
+    text: i % 3 == 0 ? hour.getHours() + '' : null
+  }
+});
+data[data.length-1].text = '24';
+
+const xScale = d3.scaleLinear([dayStart, dayStop], [4, 96]);
+const xPc = (x : Date) => xScale(x) + '%';
+
+const svgName = `${dayStr}.svg`
+const svg = d3.select(document.body)
+  .append('svg')
+  .attr('xmlns', "http://www.w3.org/2000/svg");
+
+svg.append('style').text('text { font-family: sans-serif; } line { stroke: black; }')
+
+const enterData = svg.selectAll('text')
+  .data(data)
+  .enter();
+
+enterData.append('text')
+    .attr('x', d => xPc(d.time))
+    .attr('y', '50%')
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'middle')
+    .text(d => d.text);
+
+enterData.append('line')
+  .attr('x1', d => xPc(d.time))
+  .attr('x2', d => xPc(d.time))
+  .attr('y1', '0%')
+  .attr('y2', d => d.i % 3 == 0 ? '40%' : '10%')
+
+
+
+
+svg.append('circle')
+  .attr('r', '2%')
+  .attr('cx', xPc(now))
+  .attr('cy', '40%')
+  .attr('fill', 'red')
+
+
+const svgContent = document.body.innerHTML;
+
+writeFile(svgName, svgContent);
